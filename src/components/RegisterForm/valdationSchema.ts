4772dcb12db0bc9eaede1388e77ba2b705da876b
Kowacka Malgorzata <kowackag@gmail.com>
2022-12-04 15:19:55 +0100

    Finish validation RegisterForm

--- src/components/RegisterForm/valdationSchema.ts
+++ src/components/RegisterForm/valdationSchema.ts
@@ -1,5 +1,7 @@
 import * as yup from 'yup';
 
+const phoneValidation = /^\d{9}$/;
+
 const valdationSchema = yup.object().shape({
   login: yup.string().required('Login jest wymagany'),
   password: yup.string().required('Hasło jest wymagane'),
@@ -7,7 +9,10 @@ const valdationSchema = yup.object().shape({
     .string()
     .email('Nieprawidłowy format adresu e-mail')
     .required('Adres e-mail jest wymagany'),
-  phone: yup.string().required('Nieprawidłowy numer telefonu'),
+  phone: yup
+    .string()
+    .required('Numer telefonu jest wymagany')
+    .matches(phoneValidation, 'Nieprawidłowy numer telefonu'),
   isAccepted: yup.bool().oneOf([true], 'Wymagana akceptacja regulaminu'),
 });
 
